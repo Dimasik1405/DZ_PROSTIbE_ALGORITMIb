0e491ddeb05c47861dba87071a41e0251c8eabbf
Dima Malyavka <dima_malyavka@mail.ru>
2023-12-30 19:57:23 +0300

    Решение задачи 2

---
+++ Задача 2.cs
@@ -0,0 +1,34 @@
+﻿// Домашнее задание по простым алгоритмам
+// Задача 2
+// Условие задачи: Определение координатной четверти
+using System;
+class program
+{
+static void Main()
+{
+Console.WriteLine("ВВедите координаты точек X и Y через пробел");
+string[] coordinates = Console.ReadLine().Split();
+int x = Convert.ToInt32(coordinates[0]);
+int y = Convert.ToInt32(coordinates[1]);
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("I");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("II");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("III");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("IV");
+}
+else
+{
+    Console.WriteLine("Точка лежит на оси координат");
+}
+}
+}
