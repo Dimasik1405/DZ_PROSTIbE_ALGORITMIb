09fd20339631f2ac05e9e9e5eb2b25730e6b6fc4
Dima Malyavka <dima_malyavka@mail.ru>
2024-01-01 15:53:51 +0300

    Решение задачи 3

---
+++ Задача3.cs
@@ -0,0 +1,26 @@
+﻿// Домашнее задание по простым алгоритмам
+// Задача 3
+// Условие задачи: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+using System;
+class Program
+{
+static void Main()
+{
+Console.WriteLine("Введите целое число из отрезка [10, 99]:");
+int number = Convert.ToInt32(Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else if (firstDigit < secondDigit)
+{
+    Console.WriteLine(secondDigit);
+}
+else if (firstDigit / secondDigit ==1)
+{
+Console.WriteLine(secondDigit);
+}
+}
+}
